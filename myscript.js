0ef845995f07ec55a6c2c8a7e18c12479676ad4a
Simply one <selvam.s3403@gmail.com>
2023-01-17 18:55:07 +0530

    01

---
+++ myscript.js
@@ -0,0 +1,13 @@
+var menuList=document.getElementById("menulist");
+menuList.style.maxHeight="0px";
+function togglemenu(){
+        if(menuList.style.maxHeight =="0px")
+            {
+            menuList.style.maxHeight="450px";
+            }
+        else
+            {
+                menuList.style.maxHeight="0px";
+            }
+
+}
